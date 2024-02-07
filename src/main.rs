345931a0f5b4a9f2ec8c53e991cdfdc3deea8e6e
abugger1 <92695670+abugger1@users.noreply.github.com>
2024-02-06 21:42:09 -0500

    made some changes

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 
 fn main() {
     println!("Starting Update");
-    let update = String::from("yes | sudo pacman -Syu");
+    let update = String::from("yes | sudo pacman -Sy && sudo powerpill -Su --noconfirm && pikaur -Su --noconfirm --devel && sudo flatpak update && rustup update");
     let output = Command::new("sh").arg("-c").arg(update).output();
     println!("Done");
     println!("{:?}", output)
